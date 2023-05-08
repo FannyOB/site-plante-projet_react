0b6571a64abf8d29dad85143c129fea976d55c73
Fanny Olenga Binene <folengabinene@axway.com>
2023-05-08 22:51:19 +0200

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+
+import "../styles/Banner.css"
+//import Recommendation from "./Recommendation"
+
+function Banner({ children }){
+    
+    return(
+        <div className="lmj-banner">{children}</div>
+        
+    )
+}
+
+export default Banner
---
+++ src/components/Cart.js
@@ -0,0 +1,18 @@
+function Cart(){
+    const monsteraPrice = 8
+    const ivyPrice = 10
+    const flowersPrice = 15
+
+    return(
+            <div className="lmj-cart">
+            <ul>
+                <li>Monstaera :{monsteraPrice}$</li>
+                <li>Lierre :{ivyPrice}$</li>
+                <li>Bouquet de Fleurs :{flowersPrice}$</li>
+            </ul>
+            Total: {monsteraPrice + ivyPrice + flowersPrice}$
+            </div>
+        
+    ) 
+}
+export default Cart
