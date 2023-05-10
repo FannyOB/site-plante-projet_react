1f7ed3167fe832576cdf3f6daca6acc8c9a624d4
Fanny Olenga Binene <folengabinene@axway.com>
2023-05-10 16:30:42 +0200

    add useState for Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,27 @@
 import "../styles/Cart.css"
+import { useState } from "react"
+
+/**Cart=Panier, stateful component grâce à useState
+ * cela veut dire que le composant Cart peut être re-render autant de fois que nécessaire, mais la valeur du panier sera préservée. */
 
 function Cart(){
     const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowersPrice = 15
+    /*on crée un state cart. Avec useState, nous devons déclarer en même temps une fonction pour mettre à jour ce state ( updateCart  ), 
+    et lui attribuer une valeur initiale, qui sera ici de 0 : */
+    const[cart, updateCart] = useState(0)
+   
 
     return(
-            <div className="lmj-cart">
-            <ul>
-                <li>Monstaera :{monsteraPrice}$</li>
-                <li>Lierre :{ivyPrice}$</li>
-                <li>Bouquet de Fleurs :{flowersPrice}$</li>
-            </ul>
-            Total: {monsteraPrice + ivyPrice + flowersPrice}$
+        <div className="lmj-cart">
+            <h2>Pannier</h2>
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={() => updateCart(cart + 1)}>
+                    Ajouter
+                </button>
             </div>
+            <h3>Total : {monsteraPrice * cart}€</h3>    
+        </div>
         
     ) 
 }
